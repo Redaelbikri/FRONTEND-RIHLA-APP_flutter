7dfbb98ea537721e6b5e153678e7c9c90058c1aa
Redaelbikri <REDA.ELBIKRI.ESTN24@ump.ac.ma>
2026-02-13 21:05:11 +0100

    modification de frontend 2

--- lib/data/ repositories/event_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../models/event_model.dart';
-
-abstract class EventRepository {
-  Future<List<EventModel>> getEvents();
-  Future<List<EventModel>> getEventsByCategory(String category);
-}
